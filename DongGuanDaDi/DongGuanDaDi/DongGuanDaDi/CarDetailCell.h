fd807cc4ce89c579b07b7e573a7702f4ecbd26bb
fanyunlong <fanyl@mapbar.com>
2016-07-30 15:50:28 +0800

    car detail interface

---
+++ DongGuanDaDi/DongGuanDaDi/DongGuanDaDi/CarDetailCell.h
@@ -0,0 +1,17 @@
+//
+//  CarDetailCell.h
+//  DongGuanDaDi
+//
+//  Created by fanyunlong on 7/30/16.
+//  Copyright © 2016 fanyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CarDetailCell : UITableViewCell
+@property (nonatomic, strong) IBOutlet UIImageView* thumbnail;
+@property (nonatomic, strong) IBOutlet UILabel* brand;
+@property (nonatomic, strong) IBOutlet UILabel* number;
+@property (nonatomic, strong) IBOutlet UILabel* seats;
+@property (nonatomic, strong) IBOutlet UILabel* weight;
+@end
---
+++ DongGuanDaDi/DongGuanDaDi/DongGuanDaDi/CarDetailCell.m
@@ -0,0 +1,13 @@
+//
+//  CarDetailCell.m
+//  DongGuanDaDi
+//
+//  Created by fanyunlong on 7/30/16.
+//  Copyright © 2016 fanyl. All rights reserved.
+//
+
+#import "CarDetailCell.h"
+
+@implementation CarDetailCell
+
+@end
