0f9bc39aae1795c46a6b00e40224e44efdf98f0c
fanyunlong <fanyl@mapbar.com>
2016-08-04 10:48:33 +0800

    menu detail

---
+++ DongGuanDaDi/DongGuanDaDi/DongGuanDaDi/Food.m
@@ -0,0 +1,13 @@
+//
+//  Food.m
+//  DongGuanDaDi
+//
+//  Created by fanyunlong on 8/4/16.
+//  Copyright © 2016 fanyl. All rights reserved.
+//
+
+#import "Food.h"
+
+@implementation Food
+
+@end
--- DongGuanDaDi/DongGuanDaDi/DongGuanDaDi/MemuDetailViewController.m
+++ DongGuanDaDi/DongGuanDaDi/DongGuanDaDi/MemuDetailViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MemuDetailViewController.h"
+#import "Food.h"
 
 @interface MemuDetailViewController ()
 @property (nonatomic, strong) NSMutableDictionary *dataSource;
@@ -19,18 +20,33 @@
     [super viewDidLoad];
     
     NSMutableArray* breakfast = [NSMutableArray array];
-    [breakfast addObject:@"豆浆"];
-    [breakfast addObject:@"油条"];
-    [breakfast addObject:@"胡辣汤"];
-    [breakfast addObject:@"豆汁"];
+    Food* food = [[Food alloc] init];
+    food.name = @"豆浆";
+    food.image = nil;
+    food.describe = @"一种种子榨汁而成";
+    [breakfast addObject:food];
+    Food* food2 = [[Food alloc] init];
+    food2.name = @"油条";
+    food2.image = nil;
+    food2.describe = @"用面放入锅中油炸";
+    [breakfast addObject:food2];
     NSMutableArray* lunch = [NSMutableArray array];
-    [lunch addObject:@"炸酱面"];
-    [lunch addObject:@"焦圈"];
-    [lunch addObject:@"炒肝"];
-    [lunch addObject:@"庆丰包子铺"];
+    Food* food3 = [[Food alloc] init];
+    food3.name = @"炸酱面";
+    food3.image = nil;
+    food3.describe = @"面，豆芽，萝卜，豆瓣酱卤制而成";
+    [lunch addObject:food3];
+    Food* food4 = [[Food alloc] init];
+    food4.name = @"庆丰包子铺";
+    food4.image = nil;
+    food4.describe = @"从事包子，粥，饺子文明";
+    [lunch addObject:food4];
     NSMutableArray* dinner = [NSMutableArray array];
-    [dinner addObject:@"俏江南"];
-    [dinner addObject:@"呷哺呷哺"];
+    Food* food5 = [[Food alloc] init];
+    food5.name = @"俏江南";
+    food5.image = nil;
+    food5.describe = @"某著名演员开的";
+    [dinner addObject:food5];
     self.dataSource = [NSMutableDictionary dictionary];
     [self.dataSource setObject:breakfast forKey:@"早餐"];
     [self.dataSource setObject:lunch forKey:@"午餐"];
@@ -56,8 +72,11 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"MenuDetail"];
-    NSArray* arr = [self.dataSource objectForKey:[self.dataSource.allKeys objectAtIndex:indexPath.section]];
-    cell.textLabel.text = [arr objectAtIndex:indexPath.row];
+    NSArray* arr = [self.dataSource.allValues objectAtIndex:indexPath.section];
+    Food* food = [arr objectAtIndex:indexPath.row];
+    cell.textLabel.text = food.name;
+    cell.detailTextLabel.text = food.describe;
+    cell.imageView.image = food.image;
     return cell;
 }
 @end
