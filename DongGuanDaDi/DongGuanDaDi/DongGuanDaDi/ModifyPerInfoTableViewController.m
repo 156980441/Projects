2d8efecdf1bf30dd419d889e5c713064c70b1766
fanyl <156980441@qq.com>
2016-08-30 00:40:34 +0800

    modify personal

--- DongGuanDaDi/DongGuanDaDi/DongGuanDaDi/ModifyPerInfoTableViewController.m
+++ DongGuanDaDi/DongGuanDaDi/DongGuanDaDi/ModifyPerInfoTableViewController.m
@@ -10,4 +10,36 @@
 
 @implementation ModifyPerInfoTableViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"modifyPersonal" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
 @end
