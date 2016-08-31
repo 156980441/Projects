dacf6d6e49ca8002568e275389c448c8076c40cc
fanyunlong <fanyl@mapbar.com>
2016-08-31 11:30:26 +0800

    personal info modify

--- DongGuanDaDi/DongGuanDaDi/DongGuanDaDi/ModifyPerInfoCell.m
+++ DongGuanDaDi/DongGuanDaDi/DongGuanDaDi/ModifyPerInfoCell.m
@@ -7,10 +7,24 @@
 //
 
 #import "ModifyPerInfoCell.h"
+#import "YLToast.h"
+
+#import "stdafx_DongGuanDaDi.h"
+#import "AFHTTPSessionManager.h"
 
 @implementation ModifyPerInfoCell
 
 - (IBAction)modifyPeronalInfo:(id)sender {
+    NSDictionary* dic = [NSDictionary dictionaryWithObjectsAndKeys:@"/DongGuan/",@"referer",self.qqTxtField.text,@"qq",self.phoneTxtField.text,@"phone",self.weChatTxtField.text,@"wechatId",self.phoneTxtField.text,@"phone",@"男",@"sex", nil];//这里没有性别啊？
+    [[AFHTTPSessionManager manager] POST:URL_INFO_EDIT parameters:dic success:^(NSURLSessionDataTask *task, id responseObject) {
+        NSString* result = [(NSDictionary*)responseObject objectForKey:@"result"];
+        [YLToast showWithText:result];
+        NSLog(@"Modify PerInfo succ,%@",responseObject);
+        
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        [YLToast showWithText:error.description];
+        NSLog(@"Modify PerInfo failed, %@",error);
+    }];
 }
 
 - (IBAction)cancelModifyPersonalInfo:(id)sender {
