7581a2bb69b2753bee1fc8e6435d7db09b888a2e
fanyunlong <fanyl@mapbar.com>
2016-09-06 18:03:20 +0800

    car order view

--- DongGuanDaDi/DongGuanDaDi/DongGuanDaDi/CarOrderView.h
+++ DongGuanDaDi/DongGuanDaDi/DongGuanDaDi/CarOrderView.h
@@ -8,5 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CarOrderView : UIView
+@class YLDatePicker;
+
+@interface CarOrderView : UIView 
+@property (nonatomic, strong) YLDatePicker *datePiker;
+@property (strong, nonatomic) IBOutlet UIButton *endDateBtn;
+@property (strong, nonatomic) IBOutlet UIButton *startDateBtn;
+@property (strong, nonatomic) IBOutlet UIButton *startTimeBtn;
+@property (strong, nonatomic) IBOutlet UIButton *endTimeBtn;
+@property (strong, nonatomic) IBOutlet UITextField *passenagersNunTxtField;
+- (IBAction)cancelBtnClick:(id)sender;
+- (IBAction)submitBtnClick:(id)sender;
+- (IBAction)startDateBtnClick:(id)sender;
+
+- (IBAction)endDateBtnClick:(id)sender;
+- (IBAction)startTimeBtnClick:(id)sender;
+- (IBAction)endTimeBtnClick:(id)sender;
+
+
 @end
--- DongGuanDaDi/DongGuanDaDi/DongGuanDaDi/CarOrderView.m
+++ DongGuanDaDi/DongGuanDaDi/DongGuanDaDi/CarOrderView.m
@@ -7,8 +7,29 @@
 //
 
 #import "CarOrderView.h"
+#import "YLDatePicker.h"
+#import "YLCommon.h"
+
+enum BtnTagTypes
+{
+    StartDateTag,
+    EndDateTag,
+    StartTimeTag,
+    endTimeTag
+};
+
+@interface CarOrderView () <YLDatePickerDelegate>
+
+@end
 
 @implementation CarOrderView
+
+-(void)awakeFromNib
+{
+    self.alpha = 1.0;
+    self.datePiker = [[YLDatePicker alloc] init];
+    self.datePiker.delegate = self;
+}
 
 /*
 // Only override drawRect: if you perform custom drawing.
@@ -18,4 +39,98 @@
 }
 */
 
+- (IBAction)cancelBtnClick:(id)sender
+{
+    self.datePiker.delegate = nil;
+    self.superview.alpha = 1.0;
+    [self removeFromSuperview];
+}
+
+- (IBAction)submitBtnClick:(id)sender {
+}
+
+- (IBAction)startDateBtnClick:(id)sender {
+    
+    if (self.datePiker.superview) {
+        [self.datePiker dismiss];
+    }
+    else {
+        CGRect rect = CGRectMake(0, 0, 300, 300);
+        CGPoint origin = CGPointMake(self.window.center.x - rect.size.width / 2, self.window.center.y - rect.size.height / 2);
+        [self.datePiker showInView:self.window
+                         withFrame:CGRectMake(origin.x, origin.y, rect.size.width, rect.size.height)
+                 andDatePickerMode:UIDatePickerModeDate];
+        self.datePiker.tag = StartDateTag;
+    }
+    
+}
+
+- (IBAction)endDateBtnClick:(id)sender {
+    if (self.datePiker.superview) {
+        [self.datePiker dismiss];
+    }
+    else {
+        CGRect rect = CGRectMake(0, 0, 300, 250);
+        CGPoint origin = CGPointMake(self.window.center.x - rect.size.width / 2, self.window.center.y - rect.size.height / 2);
+        [self.datePiker showInView:self.window
+                         withFrame:CGRectMake(origin.x, origin.y, rect.size.width, rect.size.height)
+                 andDatePickerMode:UIDatePickerModeDate];
+        self.datePiker.tag = EndDateTag;
+    }
+
+}
+
+- (IBAction)startTimeBtnClick:(id)sender {
+    if (self.datePiker.superview) {
+        [self.datePiker dismiss];
+    }
+    else {
+        CGRect rect = CGRectMake(0, 0, 300, 250);
+        CGPoint origin = CGPointMake(self.window.center.x - rect.size.width / 2, self.window.center.y - rect.size.height / 2);
+        [self.datePiker showInView:self.window
+                         withFrame:CGRectMake(origin.x, origin.y, rect.size.width, rect.size.height)
+                 andDatePickerMode:UIDatePickerModeTime];
+        self.datePiker.tag = StartTimeTag;
+    }
+}
+
+- (IBAction)endTimeBtnClick:(id)sender {
+    if (self.datePiker.superview) {
+        [self.datePiker dismiss];
+    }
+    else {
+        CGRect rect = CGRectMake(0, 0, 300, 250);
+        CGPoint origin = CGPointMake(self.window.center.x - rect.size.width / 2, self.window.center.y - rect.size.height / 2);
+        [self.datePiker showInView:self.window
+                         withFrame:CGRectMake(origin.x, origin.y, rect.size.width, rect.size.height)
+                 andDatePickerMode:UIDatePickerModeTime];
+        self.datePiker.tag = endTimeTag;
+    }
+}
+- (void)picker:(UIDatePicker *)picker valueChanged:(NSDate *)date
+{
+    if (picker.tag == StartTimeTag) {
+        if (date) {
+            [self.startTimeBtn setTitle:[YLCommon date2String:date] forState:UIControlStateNormal];
+        }
+    }
+    else if (picker.tag == endTimeTag)
+    {
+        if (date) {
+            [self.endTimeBtn setTitle:[YLCommon date2String:date] forState:UIControlStateNormal];
+        }
+    }
+    else if (picker.tag == EndDateTag)
+    {
+        if (date) {
+            [self.endDateBtn setTitle:[YLCommon date2String:date] forState:UIControlStateNormal];
+        }
+    }
+    else if (picker.tag == StartDateTag)
+    {
+        if (date) {
+            [self.startDateBtn setTitle:[YLCommon date2String:date] forState:UIControlStateNormal];
+        }
+    }
+}
 @end
